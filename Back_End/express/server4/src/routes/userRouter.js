e44b5c9c96f0ca9bd97c2ffbcf9561c2a9530b30
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-12-18 19:39:46 +0530

    user-details

--- Back_End/express/server4/src/routes/userRouter.js
+++ Back_End/express/server4/src/routes/userRouter.js
@@ -1,9 +1,10 @@
-import {createUser,getUser,updateUser,deleteUser} from "../controller/userController.js"
+import {signup,login,getUser,updateUser,deleteUser} from "../controller/userController.js"
 import express from "express"
 
 const user_Router = express.Router();
 
-user_Router.post("/",createUser)
+user_Router.post("/signup",signup)
+user_Router.post("/login",login)
 user_Router.get("/",getUser)
 user_Router.put("/:id",updateUser)
 user_Router.delete("/:id",deleteUser)
