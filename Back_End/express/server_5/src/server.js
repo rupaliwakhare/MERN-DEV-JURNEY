83143b214a5c7bde9097df947902b6c4bfa76e84
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-12-27 10:43:56 +0530

    server5

--- Back_End/express/server_5/src/server.js
+++ Back_End/express/server_5/src/server.js
@@ -1,19 +1,17 @@
 import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-
-dotenv.config()
+import connectDB from "../src/config/db.js";
 
 const app = express();
-const PORT = process.env.PORT || 6696
 
-app.get("/",(req,res)=>{
-    res.send("welcome to our server")
+const PORT = process.env.PORT || 6696;
+
+app.get("/",()=>{
+    res.send("welcome to our server");
+    
 })
 
 connectDB()
 
 app.listen(PORT,()=>{
-    console.log(`server is running on http://localhost:${PORT}`);
-    
+ console.log(`server is running on http://localhost:${PORT}`);
 })
