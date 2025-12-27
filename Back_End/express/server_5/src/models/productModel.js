1edd176438f4986bec918796023fd9a6dff1db9e
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-12-27 11:12:27 +0530

    user

--- Back_End/express/server_5/src/models/productModel.js
+++ Back_End/express/server_5/src/models/productModel.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    name:String,
-    email:{type:String,unique:true},
-    password: String,
-    role:{
-        type:String,
-        enum:["user","admin"],
-        default:"user"
-    }
+    title:String,
+   price:Number,
+   description:String
 })
+
+const productModel = mongoose.model("Product",productSchema)
+
+export default productModel;
